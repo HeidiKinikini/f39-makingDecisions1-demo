802f054e9b22272fe8bb63de9828bc813f5f5de7
Heidi Kinikini <Duckwitz52@gmail.com>
2023-04-11 13:30:41 -0600

    added if statement

---
+++ Tuesday-runningCode/demo/datatypes-conditionals.js
@@ -0,0 +1,29 @@
+
+let age = 24;
+const name = "Heidi";
+
+let bool = true;
+
+bool = false;
+
+//console.log(bool, age, name);
+
+// these variables have the datatype of Number
+console.log(bool, age, name);
+
+
+let num1 = 5;
+let num2 =12;
+
+/*
+this is a comment
+this is another comment
+*/
+
+if(num1 > num2) {
+    console.log(`${num1} is bigger`);
+} else if (num1 === num2) {
+    console.log("num1 and num2 are the same");
+} else{
+    console.log(num2 + " is bigger");
+}
